893ba78bb7ed8f46e7bb02f2258fde453ea95914
Sergey Kalinichenko <sergey.d.kalinichenko@gmail.com>
2021-03-21 17:17:03 +0100

    cbuffer.c v0.11

--- lib/cbuffer.c
+++ lib/cbuffer.c
@@ -1,3 +1,26 @@
+/**
+ * @file cbuffer.c
+ * @author Sergey Kalinichenko (sergey.kalinichenko@yaelev.se)
+ * @brief C assignment 02
+ * 
+ * 
+ * @version 0.11
+ * @date 2021-03-21
+ * 
+ * @copyright Copyright (c) 2021
+ * Implement a circular buffer according to the requirements below:
+ * 1.	The buffer is an array of type uint8_t
+ * 2.	The module shall have an initialization function
+ * 3.	It shall be possible to have a function to read data from the buffer
+ * 4.	It shall be possible to have a function to peek data in the buffer
+ * 5.	It shall be possible to have a function to write data to the buffer
+ * 6.	It shall be possible to have a function to return the number of avaiable elements in the buffer
+ * 7.	It shall be possible to have a function to check if the buffer is full or not
+ * 8.	Don’t change the header file or include extra libraries in the implementation file of the module.
+ * 
+ * For VG, all the requirements shall be fulfilled and the module shall be tested fully.
+ *
+ */
 #include "cbuffer.h"
 
 #if ((BUFFER_SIZE < 8U) || (BUFFER_SIZE > 32U))
@@ -11,9 +34,7 @@ static uint8_t buffer[BUFFER_SIZE] = {0};
 
 void cbuffer_init(void)
 {
-    //for (uint8_t i = 0; i < BUFFER_SIZE; buffer[i++] = 0U)
-    //    ;
-    head = tail = 0U;
+    head = tail;
     full = false;
 }
 
