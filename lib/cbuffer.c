3dd2aec1971a3f860dded613656c4acc8d875da9
Sergey Kalinichenko <sergey.d.kalinichenko@gmail.com>
2021-03-21 05:01:24 +0100

    cbuffer.c v0.1

--- lib/cbuffer.c
+++ lib/cbuffer.c
@@ -11,24 +11,51 @@ static uint8_t buffer[BUFFER_SIZE] = {0};
 
 void cbuffer_init(void)
 {
+    //for (uint8_t i = 0; i < BUFFER_SIZE; buffer[i++] = 0U)
+    //    ;
+    head = tail = 0U;
+    full = false;
 }
 
 void cbuffer_write(uint8_t value)
 {
+    buffer[tail] = value;
+    tail = (tail + 1) % BUFFER_SIZE;
+    if (full)
+    {
+        head = tail;
+    }
+    else if (tail == head)
+    {
+        full = true;
+    }
 }
 
 uint8_t cbuffer_read(void)
 {
+    uint8_t value = 0;
+
+    if (cbuffer_available())
+    {
+        value = buffer[head];
+        full = false;
+        head = (head + 1) % BUFFER_SIZE;
+    }
+
+    return value;
 }
 
 bool cbuffer_isfull(void)
 {
+    return full;
 }
 
 uint8_t cbuffer_peek(void)
 {
+    return !cbuffer_available() ? 0 : buffer[head];
 }
 
 uint8_t cbuffer_available(void)
 {
+    return full ? BUFFER_SIZE : (tail - head) % BUFFER_SIZE;
 }
